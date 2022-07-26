de15668ded0f26996c0e040324fb059c2ff3b9c5
July2220 <tjjsilwolf@gmail.com>
2022-07-26 22:33:07 +0800

    Memorize first commit

---
+++ MyFirstRepo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MyFirstRepo
+//
+//  Created by 陶佳佳 on 2022/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .font(.headline)
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirstRepo/MyFirstRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstRepoApp.swift
+//  MyFirstRepo
+//
+//  Created by 陶佳佳 on 2022/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
